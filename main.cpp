29963e24201e44a6d99b743fa994841edada9dc9
rodericksolana <rodericksolana@gmail.com>
2016-02-23 13:27:32 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -67,6 +67,13 @@ int main(){
 			break;
 		default: cout << "El valor ingresado no esta en el menu" << endl;
 		}
+
+	for (int i = 0; i < 9; i++){
+		matResultado[i][0] = 0;
+		restaMatInicialFinal[i][0]=0;
+	
+}
+
 		cout << endl << "Selecciona una opcion: \n";
 		cout << "1  Introducir Matriz Inicial y Final\n";
 		cout << "2  Generar Matriz Inicial y Final\n";
@@ -131,86 +138,32 @@ void resuelveProblema(int restaMatInicialFinal[9][1], int matInicial[9][1], int 
 void multiplicaMatrices(int restaMatInicialFinal[9][1], int matResultados[9][1], int mod){
 	// Declaracion de la matriz de 9x9, A inversa (A matriz de las diferentes transformaciones que puede sufrir la matriz)
 	int Ainv[9][9];
-	Ainv[0][0] = 1;
-	Ainv[1][0] = 1;
-	Ainv[2][0] = 1;
-	Ainv[3][0] = 1;
-	Ainv[4][0] = 1;
-	Ainv[5][0] = 0;
-	Ainv[6][0] = 1;
-	Ainv[7][0] = 0;
+	/*columna 1*/
+	Ainv[0][0] = 1;	Ainv[1][0] = 1;	Ainv[2][0] = 1;	Ainv[3][0] = 1;	Ainv[4][0] = 1;	Ainv[5][0] = 0;	Ainv[6][0] = 1;	Ainv[7][0] = 0;
 	Ainv[8][0] = 0;
-	Ainv[0][1] = 0;
-	Ainv[1][1] = 1;
-	Ainv[2][1] = 0;
-	Ainv[3][1] = 1;
-	Ainv[4][1] = 0;
-	Ainv[5][1] = 1;
-	Ainv[6][1] = 1;
-	Ainv[7][1] = 0;
+	/*columna 2*/	
+	Ainv[0][1] = 0;	Ainv[1][1] = 1;	Ainv[2][1] = 0;	Ainv[3][1] = 1;	Ainv[4][1] = 0;	Ainv[5][1] = 1;	Ainv[6][1] = 1;	Ainv[7][1] = 0;
 	Ainv[8][1] = 1;
-	Ainv[0][2] = 1;
-	Ainv[1][2] = 1;
-	Ainv[2][2] = 1;
-	Ainv[3][2] = 0;
-	Ainv[4][2] = 1;
-	Ainv[5][2] = 1;
-	Ainv[6][2] = 0;
-	Ainv[7][2] = 0;
+	/*columna 3*/
+	Ainv[0][2] = 1;	Ainv[1][2] = 1;	Ainv[2][2] = 1;	Ainv[3][2] = 0;	Ainv[4][2] = 1;	Ainv[5][2] = 1;	Ainv[6][2] = 0;	Ainv[7][2] = 0;
 	Ainv[8][2] = 1;
-	Ainv[0][3] = 0;
-	Ainv[1][3] = 1;
-	Ainv[2][3] = 1;
-	Ainv[3][3] = 1;
-	Ainv[4][3] = 0;
-	Ainv[5][3] = 0;
-	Ainv[6][3] = 0;
-	Ainv[7][3] = 1;
+	/*columna 4*/
+	Ainv[0][3] = 0;	Ainv[1][3] = 1;	Ainv[2][3] = 1;	Ainv[3][3] = 1;	Ainv[4][3] = 0;	Ainv[5][3] = 0;	Ainv[6][3] = 0;	Ainv[7][3] = 1;
 	Ainv[8][3] = 1;
-	Ainv[0][4] = 0;
-	Ainv[1][4] = 1;
-	Ainv[2][4] = 0;
-	Ainv[3][4] = 1;
-	Ainv[4][4] = 1;
-	Ainv[5][4] = 1;
-	Ainv[6][4] = 0;
-	Ainv[7][4] = 1;
+	/*columna 5*/
+	Ainv[0][4] = 0;	Ainv[1][4] = 1;	Ainv[2][4] = 0;	Ainv[3][4] = 1;	Ainv[4][4] = 1;	Ainv[5][4] = 1;	Ainv[6][4] = 0;	Ainv[7][4] = 1;
 	Ainv[8][4] = 0;
-	Ainv[0][5] = 1;
-	Ainv[1][5] = 1;
-	Ainv[2][5] = 0;
-	Ainv[3][5] = 0;
-	Ainv[4][5] = 0;
-	Ainv[5][5] = 1;
-	Ainv[6][5] = 1;
-	Ainv[7][5] = 1;
+	/*columna 6*/
+	Ainv[0][5] = 1;	Ainv[1][5] = 1;	Ainv[2][5] = 0;	Ainv[3][5] = 0;	Ainv[4][5] = 0;	Ainv[5][5] = 1;	Ainv[6][5] = 1;	Ainv[7][5] = 1;
 	Ainv[8][5] = 0;
-	Ainv[0][6] = 1;
-	Ainv[1][6] = 0;
-	Ainv[2][6] = 0;
-	Ainv[3][6] = 1;
-	Ainv[4][6] = 1;
-	Ainv[5][6] = 0;
-	Ainv[6][6] = 1;
-	Ainv[7][6] = 1;
+	/*columna 7*/
+	Ainv[0][6] = 1;	Ainv[1][6] = 0;	Ainv[2][6] = 0;	Ainv[3][6] = 1;	Ainv[4][6] = 1;	Ainv[5][6] = 0;	Ainv[6][6] = 1;	Ainv[7][6] = 1;
 	Ainv[8][6] = 1;
-	Ainv[0][7] = 1;
-	Ainv[1][7] = 0;
-	Ainv[2][7] = 1;
-	Ainv[3][7] = 1;
-	Ainv[4][7] = 0;
-	Ainv[5][7] = 1;
-	Ainv[6][7] = 0;
-	Ainv[7][7] = 1;
+	/*columna 8*/
+	Ainv[0][7] = 1;	Ainv[1][7] = 0;	Ainv[2][7] = 1;	Ainv[3][7] = 1;	Ainv[4][7] = 0;	Ainv[5][7] = 1;	Ainv[6][7] = 0;	Ainv[7][7] = 1;
 	Ainv[8][7] = 0;
-	Ainv[0][8] = 0;
-	Ainv[1][8] = 0;
-	Ainv[2][8] = 1;
-	Ainv[3][8] = 0;
-	Ainv[4][8] = 1;
-	Ainv[5][8] = 1;
-	Ainv[6][8] = 1;
-	Ainv[7][8] = 1;
+	/*columna 9*/
+	Ainv[0][8] = 0;	Ainv[1][8] = 0;	Ainv[2][8] = 1;	Ainv[3][8] = 0;	Ainv[4][8] = 1;	Ainv[5][8] = 1;	Ainv[6][8] = 1;	Ainv[7][8] = 1;
 	Ainv[8][8] = 1;
 
 	// Guardar el resultado de la multimplicacion entre la inversa de A y la matriz suma.
@@ -220,7 +173,6 @@ void multiplicaMatrices(int restaMatInicialFinal[9][1], int matResultados[9][1],
 			matResultados[i][0] = (matResultados[i][0] + (Ainv[i][j] * restaMatInicialFinal[j][0])) % mod;
 		}
 	}
-
 
 
 	// entero de ayuda para la impresino del vector como matriz de 3x3
@@ -237,7 +189,7 @@ void multiplicaMatrices(int restaMatInicialFinal[9][1], int matResultados[9][1],
 		conta++;
 	}
 
-	cout << "Para resolver esa matriz basta con presionar las casillas señaladas el numero de veces que aparece (1 o 2)\n";
+	cout << endl << endl <<"Para resolver esa matriz basta con presionar las casillas señaladas el numero de veces que aparece (1 o 2)\n";
 
 
 }
